f58ce721be5e58f290e36f9f927f472eb27d728a
ved98233-cmd <ved98233@gmail.com>
2026-02-01 12:57:05 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "Rutu",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? 💝",
+    pageTitle: "Will You Be My Valentine Cutu? 💝",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
